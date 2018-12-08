620ba07d6c0c0fee04bab58e2ae24e880900349a
jungbomp <jungbomp@usc.edu>
2018-12-08 02:56:35 -0800

    Initialize spatial and temporal anti-aliasing

---
+++ Spatial-Temporal-Anti-Aliasing/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Image.rc
+//
+#define IDC_MYICON                      2
+#define IDD_IMAGE_DIALOG                102
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDS_HELLO                       106
+#define IDI_IMAGE                       107
+#define IDI_SMALL                       108
+#define IDC_IMAGE                       109
+#define IDR_MAINFRAME                   128
+#define ID_MODIFY_IMAGE                 32771
+#define ID_IMAGE_OPEN                   32772
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
